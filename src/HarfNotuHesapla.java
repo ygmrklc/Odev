d38d7c814ebe93e3e0f78e67dea8cf811d9a519c
Yagmur Kilic <Yagmur.Kilic@sahibinden.com>
2022-08-06 22:22:25 +0300

    first checkin

---
+++ src/HarfNotuHesapla.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class HarfNotuHesapla {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Scanner i = new Scanner(System.in);
+        System.out.println("Lutfen isminizi giriniz.");
+        String isim = i.nextLine();
+        System.out.println("Lutfen quiz notunuzu giriniz.");
+        int quiz_not = i.nextInt();
+        System.out.println("Lutfen vize notunuzu giriniz.");
+        int vize_not = i.nextInt();
+        System.out.println("Lutfen final notunuzu giriniz.");
+        int final_not = i.nextInt();
+
+        double not_ortalamasi = (quiz_not * 0.1) + (vize_not * 0.30) + (final_not * 0.6);
+        boolean gecti_mi = true;
+        if (not_ortalamasi >= 85) {
+            System.out.println("Sayın " + isim + " Not Ortalamanız: AA");
+        } else if (not_ortalamasi >= 70) {
+            System.out.println("Sayın " + isim + " Not Ortalamanız: BB");
+        } else if (not_ortalamasi >= 60) {
+            System.out.println("Sayın " + isim + " Not Ortalamanız: CC");
+        } else if (not_ortalamasi >= 45) {
+            System.out.println("Sayın " + isim + " Not Ortalamanız: DD");
+        } else {
+            gecti_mi = false;
+            System.out.println("Sayın " + isim + " Not Ortalamanız: FF");
+        }
+        if (gecti_mi == true) {
+            System.out.println("Tebrikler, sınıfı geçtiniz.");
+        } else {
+            System.out.println("Üzgünüz, sınıfta kaldınız.");
+        }
+    }
+}
